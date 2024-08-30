3fe3aa059041295b8efa797a5f818dcfee9727a3
Nhuz11 <nhuzbajra59@gmail.com>
2024-08-30 16:33:50 +0545

    backend: validation

--- Backend/Routes/Auth.js
+++ Backend/Routes/Auth.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const Product = require('../Model/Product')
+const User = require('../Model/User')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
-router.get("/",(req,res)=>{
+const { body, validationResult } = require('express-validator')
+
+router.get("/", (req, res) => {
     obj = {
         a: "Nhuz",
         b: "Bajra"
@@ -9,11 +14,40 @@ router.get("/",(req,res)=>{
     res.send(obj)
 })
 
-router.post("/createUser", (req,res)=>{
+router.post("/createUser", [// username must be an email
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    // password must be at least 5 chars long
+    body('password').isLength({ min: 5 })], 
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        let user=await User.findOne({email:req.body.email})
+        if(user){
+            return res.status(400).json({error:"email already exists."})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const securePass = await bcrypt.hash(req.body.password,salt) 
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: securePass,
+        }).then(user => res.json(user));
+        // console.log(req.body)
+        // res.send(req.body)
+        // const user= User(req.body)
+        // user.save()
+    })
+
+router.post("/createProduct", (req, res) => {
     console.log(req.body)
     res.send(req.body)
-    const user= User(req.body)
-    user.save()
+    const product = Product(req.body)
+    product.save()
 })
 
 
