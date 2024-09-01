ad9514bd732f5f10930af792bc3a1c8ad1176b10
Nhuz11 <nhuzbajra59@gmail.com>
2024-09-01 16:11:53 +0545

    login successful, jsonwebtoken

--- Backend/Routes/Auth.js
+++ Backend/Routes/Auth.js
@@ -3,49 +3,96 @@ const Product = require('../Model/Product')
 const User = require('../Model/User')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+console.log(JWT_SECRET)
 
 const { body, validationResult } = require('express-validator')
 
-router.get("/", (req, res) => {
-    obj = {
-        a: "Nhuz",
-        b: "Bajra"
-    }
-    obj = {
-        a: "Nhuz",
-        b: "Bajra"
-    }
-    res.send(obj)
-})
-
-router.post("/createUser", [// username must be an email
+//creating user with using post "api/auth/createuser"
+router.post("/createUser", [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
-    // password must be at least 5 chars long
-    body('password').isLength({ min: 5 })], 
-    async(req, res) => {
+    body('password').isLength({ min: 5 })],
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        let user=await User.findOne({email:req.body.email})
-        if(user){
-            return res.status(400).json({error:"email already exists."})
-        }
+        try {
+            let user = await User.findOne({ email: req.body.email })
+            if (user) {
+                return res.status(400).json({ error: "email already exists." })
+            }
 
-        const salt = await bcrypt.genSalt(10)
-        const securePass = await bcrypt.hash(req.body.password,salt) 
-        await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: securePass,
-        }).then(user => res.json(user));
+            const salt = await bcrypt.genSalt(10)
+            const securePass = await bcrypt.hash(req.body.password, salt)
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: securePass,
+            })
+
+        const data = {
+            user:{
+                id: user.id
+            }
+        }    
+        const authToken = jwt.sign(data,JWT_SECRET)
+        console.log(authToken)
+        res.json({user, authToken})
+        } catch (error) {
+            res.status(500).send("Internal server error")
+        }
         // console.log(req.body)
         // res.send(req.body)
         // const user= User(req.body)
         // user.save()
     })
+
+
+//login user with using post "api/auth/login"
+router.post("/login", [
+    
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+
+        const {email, password} = req.body
+        try{
+            let user = await User.findOne({email})
+            if(!user){
+                return res.status(400).json({errors: "Invalid Credentials."})
+            }
+            const passwordCompare = await bcrypt.compare(password, user.password)
+            if(!passwordCompare){
+                return res.status(400).json({errors: "Invalid Credentials."})
+            }
+            const data = {
+                user:{
+                    id: user.id
+                }
+            }    
+            const authToken = jwt.sign(data,JWT_SECRET)
+            console.log(authToken)
+            res.json({user, authToken})
+
+        }catch(error){
+            res.status(500).send("Internal server error")
+        }
+    }
+)
+
+
+
 
 router.post("/createProduct", (req, res) => {
     console.log(req.body)
@@ -57,5 +104,31 @@ router.post("/createProduct", (req, res) => {
 
 module.exports = router
 
-//44454488adsfasdfasdf
-//dsafasdfasdfasdfasdfasdfasdfasdfasdf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.get("/", (req, res) => {
+//     obj = {
+//         a: "Nhuz",
+//         b: "Bajra"
+//     }
+
+//     res.send(obj)
+// })
