50ceed2c666d02bc9c7dc0daff8b8b9da4b22bac
Nhuz11 <nhuzbajra59@gmail.com>
2024-10-06 22:56:25 +0545

    final

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -56,6 +56,7 @@ function App() {
 
           <Routes>
             
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/card" element={<Card  />} />
             <Route path="/car" element={<Car />} />
